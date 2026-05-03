9093886030b29d95806921b90f5be80dab53bd17
ritik574-coder <ritik74820@gmail.com>
2026-05-03 19:58:06 +0530

    python script are creating now it's time to commit it

--- python Libraries/databse/sql_server/database.py
+++ python Libraries/databse/sql_server/database.py
@@ -1,10 +1,10 @@
-import pyodbc 
-import os
+import pyodbc
+import os 
 
-def connnect_to_databse(database="master") :
+def connect_to_database(database='master') :
+    print("connecting to sql server")
 
-    print("connecting to SQL Server........")
-    try :
+    try : 
         conn = pyodbc.connect(
             "DRIVER={ODBC Driver 18 for SQL Server};"
             "SERVER=localhost,1433;"
@@ -13,86 +13,14 @@ def connnect_to_databse(database="master") :
             f"PWD={os.getenv('DB_PASSWORD')};"
             "Encrypt=yes;"
             "TrustServerCertificate=yes;"
-
         )
-        print("database connectiong successfully :) ")
+
+        print("Database connecting successfully :) ")
         return conn
-    
-    except pyodbc.Error as e :
-        print(f"Databse connectiong faild {e}")
-        raise
 
-def create_database(conn):
-    try :
+    except pyodbc.Error as e : 
+        print(f"databse connectin faild {e}")
+        raise 
 
-        conn.autocommit = True
-        print(">> dropping databse if exists...")
-
-        cursor = conn.cursor()
-        cursor.execute("""
-                IF EXISTS (SELECT name FROM sys.databases WHERE name = 'PythonDB')
-                BEGIN
-                    ALTER DATABASE PythonDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE ;
-                    DROP DATABASE PythonDB ;
-                END ;
-        """)
-
-        print("creating databse PythonDB")
-
-        cursor.execute("CREATE DATABASE PythonDB ;")
-
-    except pyodbc.Error as e :
-        print(f"Error Occurred while creating database {e}")
-        raise
-
-def create_schema():
-    try :
-        print("Creating schema based on the Medallion architecture")
-
-        conn = connnect_to_databse("PythonDB")
-        cursor = conn.cursor()
-
-        print(">> creating bronze schema.....")
-        cursor.execute("""
-                IF NOT EXISTS(SELECT * FROM sys.schemas WHERE name = 'bronze')
-                BEGIN 
-                       EXEC('CREATE SCHEMA bronze AUTHORIZATION dbo') ;
-                END ;
-        """)
-
-        print(">> creating silver schema.....")
-        cursor.execute("""
-                IF NOT EXISTS(SELECT * FROM sys.schemas WHERE name = 'silver')
-                BEGIN 
-                       EXEC('CREATE SCHEMA silver AUTHORIZATION dbo') ;
-                END ;
-        """)
-
-        print(">> creating gold schema.....")
-        cursor.execute("""
-                IF NOT EXISTS(SELECT * FROM sys.schemas WHERE name = 'gold')
-                BEGIN 
-                       EXEC('CREATE SCHEMA gold AUTHORIZATION dbo') ;
-                END ;
-        """)
-
-        conn.commit()
-        conn.close()
-
-    except pyodbc.Error as e :
-        print(f"Error occurred during schema creating : {e}")
-        raise
-
-def main() :
-    try :
-        conn = connnect_to_databse()
-        create_database(conn)
-
-        conn.close()
-        create_schema()
-
-    except Exception as e :
-        print(f"An error occurred during excution : {e}")
-
-if __name__ == "__main__" :
-    main()
+def create_database() :
+    print("creating database for preastic")
