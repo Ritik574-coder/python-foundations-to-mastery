a9b7c7e7404925a24db437757d133e627924f8c3
ritik574-coder <ritik74820@gmail.com>
2026-05-07 16:19:11 +0530

    adding a project data warehousing using python code

--- python Libraries/databse/sql_server/exployre.sql
+++ python Libraries/databse/sql_server/exployre.sql
@@ -1,3 +1,24 @@
-SELECT * FROM INFORMATION_SCHEMA.SCHEMATA ;
+-- creating database 
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'PythonDB')
+BEGIN 
+    ALTER DATABASE PythonDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE ;
+    DROP DATABASE PythonDB ;
+END ;
 
-DROP DATABASE PythonDB ;
+-- creatin bronze schema 
+IF NOT EXISTS(SELECT 1 FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+    EXEC('CREATE SCHEMA bronze AUTHORIZATION dbo') ;
+END ;
+
+-- creatin silver scheam 
+IF NOT EXISTS(SELECT 1 FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+    EXEC('CREATE SCHEMA bronze AUTHORIZATION dbo') ;
+END ;
+
+-- creating gold schema 
+IF NOT EXISTS(SELECT 1 FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+    EXEC('CREATE SCHEMA bronze AUTHORIZATION dbo') ;
+END ;
